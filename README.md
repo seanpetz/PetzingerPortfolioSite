1092fe7318ce30b2be09475bb26435f3c1960493
Sean Petzinger <spetzinger926@gmail.com>
2022-07-28 15:18:11 -0400

    used javascript to populate fields on contact form

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ Summer 2022
 
 My project is a visually appealing portfolio site for my two possible career paths. Graphic Designer and Web Developer. 
 Open the index.html file to get started...
-•	I used media queries to adjust my layout based on size
-•	CSS Feature: Navigation menu that expands and collapses 
-•	CSS Feature: Flexbox organizes content based on media size
-•	Javascript Feature: Created a “Contact Me” form
-•	Javascript Feature: Created a lightbox on the “Graphic Designer” page using javascript
+
+I have included the following:
+•	You must include a minimum of 2 media queries that create a better layout/user experience
+•	I have made more than 5 commits to GitHub
+•	A navigation menu that expands and collapses properly at desktop and mobile sizes, such as by switching between vertical and horizontal styles, or is opened and closed by clicking the “hamburger” icon
+•	Use Flexbox or CSS Grid to organize content areas based on mobile or desktop views. Simply applying a basic flex property so that text wraps as you change screen sizes does not count. You must actually rearrange content or perform some more advanced feature. For example, swapping from a single column layout to a two-column layout on a desktop.
+•	Create at least 3 variables (var, const, or let), set their values in JavaScript, and display the values somewhere on your page - these are the headings for the fields on the contact form.
+•	I did create the contact form with Javascript however I was not able to pull the information back to populate the "alert"
+•	I created the portfolio modal using javascript on the Graphic Designer page.
--- form.js
+++ form.js
@@ -1,55 +1,52 @@
 //Navigation//
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-
-
-
+}
 
 // show a message with a type of the input
 function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector("small");
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? "success" : "error";
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
 
 function showSuccess(input) {
-	return showMessage(input, "", true);
+  return showMessage(input, "", true);
 }
 
 function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
+  if (input.value.trim() === "") {
+    return showError(input, message);
+  }
+  return showSuccess(input);
 }
 
 function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
 
 const form = document.querySelector("#contact");
@@ -58,17 +55,37 @@ const NAME_REQUIRED = "Please enter your name";
 const EMAIL_REQUIRED = "Please enter your email";
 const EMAIL_INVALID = "Please enter a correct email address format";
 
-
 form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
+  // stop form submission
+  event.preventDefault();
 
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		let username= document.getElementById{{username.nameValid}}
-		alert(`Thank you ${username}! I will get back to you soon!`);
-	}
+  // validate the form
+  let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+  let emailValid = validateEmail(
+    form.elements["email"],
+    EMAIL_REQUIRED,
+    EMAIL_INVALID
+  );
+  // if valid, submit the form.
+  if (nameValid && emailValid) {
+    alert(`Thank you! I will get back to you soon!`);
+  }
 });
+
+let myName = "Sean Petzinger";
+document.getElementById("myName").innerHTML = myName;
+
+let myEmail = "seanpetzinger@gmail.com";
+document.getElementById("myEmail").innerHTML = myEmail;
+
+let myPhone = "(859) 468-2839";
+document.getElementById("myPhone").innerHTML = myPhone;
+
+let yourName = "Name";
+document.getElementById("firstcell").innerHTML = yourName;
+
+let yourEmail = "Email";
+document.getElementById("secondcell").innerHTML = yourEmail;
+
+let yourMessage = "Message";
+document.getElementById("thirdcell").innerHTML = yourMessage;
